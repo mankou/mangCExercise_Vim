d0e8f04bd21b341c49f9b25bdc382272fef7838f
man1m <ningma003@gmail.com>
2012-07-16 23:33:56 +0800

    希尔排序完成

--- Sort/shellSort/main.c
+++ Sort/shellSort/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include	<stdlib.h>
-# define N 5
+# define N 5                                    /*代表数组记录个数  */
 
 /* 
  * ===  FUNCTION  ======================================================================
@@ -11,18 +11,19 @@
 	int
 main ( int argc, char *argv[] )
 {
-//	shellSort(int *，int);
-	int a[N+1]={0,1,8,9,12,5};
+	void shellSort(int * a,int n);
+	int a[N+1]={0,13,10,9,12,5};                /* a[0]不存储数据，用于监督哨 */
 	int i=0;
 	printf("希而排序示例：\n");
 	printf("原始数组：\n");
-	for(i=1;i<N;i++)
+	for(i=1;i<=N;i++)
 		printf("%d ",a[i]);
 	
-//	shellSort(a,N)                              /* 希尔排序 */
+	shellSort(a,N);                              /* 希尔排序 */
 	printf("\n希而排序后结果：\n");
-	for(i=1;i<N;i++)
+	for(i=1;i<=N;i++)
 		printf("%d ",a[i]);
 
+	getchar();getchar();
 	return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
