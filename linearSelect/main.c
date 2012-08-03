5025b4d5b911a407a3c2b8823981fe95d6bc5654
man1m <ningma003@gmail.com>
2012-08-03 22:53:38 +0800

    完成线性选择的代码 下一步写注释

--- linearSelect/main.c
+++ linearSelect/main.c
@@ -12,25 +12,27 @@
 main ( int argc, char *argv[] )
 {
 	int linearSelect(int *a,int h,int e,int k);
-	int a[N]={8,5,1,3,7,6};
-	int b[N]={0};
+	int a[N]={2,5,1,3,7,6};
 	int k;
 	int i,m;
 	
-	printf("线性选择示例\n");
+	printf("线性选择示例,本程序将帮助您找第k小的值\n");
 	printf("输入k:");
 	scanf("%d",&k);
 
-	printf("数组序列如下：\n");
+	printf("\n数组初始序列如下：\n");
 	for (i = 0; i < N; i++) {
 		printf("%d ",a[i]);
 	}
-	for (i = 0; i <N; i++) {
-			b[i]=a[i];
-		}
-	m=linearSelect(b,0,N-1,k);
-	printf("\n经过线性选择后m=%d\n",m);
-	printf("\n第%d小数为%d",k,b[m]);
+	printf("\n");
+
+	if (k<N) {
+		m=linearSelect(a,0,N-1,k);
+		printf("\n第%d小数为%d\n",k,a[m]);
+	}
+	else {
+		printf("输入错误\n");
+	}
 
 	return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
