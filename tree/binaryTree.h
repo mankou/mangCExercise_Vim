f828a025d0a7e85193cda47c042d9fb044cb5ee3
lab_mang <ningma003@gmail.com>
2012-08-14 11:41:44 +0800

    二叉搜索树完成初步代码　写一步写注释　写find函数

---
+++ tree/binaryTree.h
@@ -0,0 +1,16 @@
+#define TREE_TYPE int
+
+struct binaryTree {
+	TREE_TYPE value;
+	struct biaryTree * left;
+	struct biaryTree * right;
+} /* optional variable list */;
+typedef struct binaryTree BinaryTree;
+
+void insert(BinaryTree **current,TREE_TYPE value);
+
+
+BinaryTree *find();
+
+
+void inOrderTraverse(BinaryTree *);
